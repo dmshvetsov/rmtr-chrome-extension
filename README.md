095dc3fd40d601a587e5303a1c7a54459c634033
Dmitry Shvetsov <shvetsovdm@gmail.com>
2020-11-10 17:41:12 +1000

    feat: make rmtr MVP

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
-# Getting Started with Create React App
+# rmtr
+
+New tab extension for Google Chrome browser. Displays time in diferent time zones.
+
+[Color pallete used](https://coolors.co/fffcf2-ccc5b9-403d39-252422-eb5e28)
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Setup and Usage
+
+Currently this extenstion available to install only manually.
+
+1. Clone/download the project to your hard drive.
+1. Open `scr/App.js` and edit `locations` variable.
+1. Run `yarn && yarn build`
+1. Open "Manage extensions" in Google Chrome.
+1. Click "Load unpacked".
+1. In the opened window find the root of the project and click "select".
+1. Open new tab to see the extension in action.
 
 ## Available Scripts
 
--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React from 'react';
+import moment from 'moment-timezone';
 import './App.css';
+
+const DATE_TIME_FORMAT = `${moment.HTML5_FMT.DATE} ${moment.HTML5_FMT.TIME}`;
+const TIME_FORMAT_WITH_SECONDS = 'LTS';
+const TIME_FORMAT = 'LT';
+
+const locations = [
+  { timeZone: 'Asia/Vladivostok', isCurrent: true },
+  { timeZone: 'Asia/Hong_Kong', isCurrent: false },
+  { timeZone: 'America/Toronto', isCurrent: false }
+];
+
+function formatLocationName(str) {
+  return str.replace(/_/, ' ');
+}
+
+function Time({ timeZone, isCurrent }) {
+  const [dateTime, setDateTime] = React.useState(moment().tz(timeZone));
+  React.useEffect(() => setDateTime(moment().tz(timeZone)), [dateTime]);
+  return (
+    <div className="App-location">
+      {formatLocationName(timeZone)}
+      <br/>
+      <time
+        className="App-time"
+        datetime={dateTime.format(DATE_TIME_FORMAT)}
+      >
+        {dateTime.format(isCurrent ? TIME_FORMAT_WITH_SECONDS : TIME_FORMAT)}
+      </time>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {locations.map((item) => <Time {...item} />)}
+      </header>
+      <footer className="App-footer">
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="App-link App-author"
+          href="https://dmitryshvetsov.com/?utm_source=rmtr&utm_medium=chrome-extension"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Dmitry Shvetsov
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
