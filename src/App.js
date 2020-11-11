addb2ee6e1601b34cb777d009c331298abf4093d
Dmitry Shvetsov <shvetsovdm@gmail.com>
2020-11-11 17:53:54 +1000

    feat: adding new time zones
    This is a sub feature that requires other improvements
    such as memoizing user selected time zones and better
    UI and UX.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,125 @@
 import React from 'react';
+import Select from 'react-select';
 import moment from 'moment-timezone';
 import './App.css';
 
 const DATE_TIME_FORMAT = `${moment.HTML5_FMT.DATE} ${moment.HTML5_FMT.TIME}`;
 const TIME_FORMAT_WITH_SECONDS = 'LTS';
 const TIME_FORMAT = 'LT';
+const SECOND = 1000;
 
-const locations = [
-  { timeZone: 'Asia/Vladivostok', isCurrent: true },
-  { timeZone: 'Asia/Hong_Kong', isCurrent: false },
-  { timeZone: 'America/Toronto', isCurrent: false }
-];
+function createLocation({ timeZone, isCurrent }) {
+  if (!moment.tz.zone(timeZone)) {
+    throw new Error(`Wrong time zone ${timeZone}`);
+  }
+
+  return {
+    timeZone,
+    isCurrent
+  };
+}
+
+function prepareLocations(locations) {
+  return locations.filter((item) => item && item.timeZone);
+}
+
+function getCurrentLocation() {
+  return createLocation({
+    timeZone: moment.tz.guess(),
+    isCurrent: true
+  });
+}
 
 function formatLocationName(str) {
   return str.replace(/_/, ' ');
 }
 
-function Time({ timeZone, isCurrent }) {
-  const [dateTime, setDateTime] = React.useState(moment().tz(timeZone));
-  React.useEffect(() => setDateTime(moment().tz(timeZone)), [dateTime]);
+function Time({ location }) {
+  const [dateTime, setDateTime] = React.useState(moment().tz(location.timeZone));
+  React.useEffect(() => {
+    const interval = setInterval(
+      () => setDateTime(moment().tz(location.timeZone)),
+      SECOND
+    );
+    return () => clearInterval(interval);
+  }, [location.timeZone]);
   return (
     <div className="App-location">
-      {formatLocationName(timeZone)}
+      {formatLocationName(location.timeZone)}
       <br/>
       <time
         className="App-time"
-        datetime={dateTime.format(DATE_TIME_FORMAT)}
+        dateTime={dateTime.format(DATE_TIME_FORMAT)}
       >
-        {dateTime.format(isCurrent ? TIME_FORMAT_WITH_SECONDS : TIME_FORMAT)}
+        {dateTime.format(location.isCurrent ? TIME_FORMAT_WITH_SECONDS : TIME_FORMAT)}
       </time>
     </div>
   );
 }
 
+function Button({ text, ...rest }) {
+  return <button className="App-button" {...rest}>{text}</button>;
+}
+
+function ZonePickField(props) {
+  const inputRef = React.useRef(null);
+  const options = moment.tz.names().map((timeZoneName) => ({
+    value: timeZoneName,
+    label: timeZoneName
+  }));
+  React.useEffect(() => inputRef.current.focus());
+  return (
+    <Select 
+      ref={inputRef}
+      placeholder="Type to search"
+      menuPlacement="auto"
+      menuPosition="fixed"
+      options={options}
+      isSearchable
+      {...props}
+    />
+  );
+}
+
+function AddLocationForm({ submit }) {
+  const [isEdited, setEdited] = React.useState(false);
+
+  const handleChange = (option) => {
+    submit(option.value);
+    setEdited(!isEdited);
+  }
+
+
+  if (isEdited) {
+    return (
+      <div style={{ width: '350px', textAlign: 'left' }}>
+        <ZonePickField onChange={handleChange} />
+        <Button text="cancel" onClick={() => setEdited(!isEdited)}/>
+      </div>
+    );
+  }
+  return <Button text="+" onClick={() => setEdited(!isEdited)}/>;
+}
+
 function App() {
+  const [locations, setLocations] = React.useState(
+    prepareLocations([
+      getCurrentLocation()
+    ])
+  );
+
+  const handleAddLocation = (newLocationName) => {
+    setLocations([
+      ...locations,
+      createLocation({ timeZone: newLocationName })
+    ]);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        {locations.map((item) => <Time {...item} />)}
+        {locations.map((item) => <Time key={item.timeZone} location={item} />)}
+        <AddLocationForm submit={handleAddLocation} />
       </header>
       <footer className="App-footer">
         <a
