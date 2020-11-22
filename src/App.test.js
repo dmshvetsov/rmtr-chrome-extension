41166e23da480421f9a2fa839651285966150cec
Dmitry Shvetsov <shvetsovdm@gmail.com>
2020-11-22 17:25:33 +1000

    tests: fix the single test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Dmitry Shvetsov link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Dmitry Shvetsov/i);
   expect(linkElement).toBeInTheDocument();
 });
